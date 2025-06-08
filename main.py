cd7d0950ab5b8fbf2b8d2359a7939f9aa4ad8788
VANSHAJB10 <vanshajbajaj1002@gmail.com>
2025-06-08 13:00:21 +0530

    [Vbajaj] Resolving vulnerabilities of exposed API Key.
    - Removed hardcoded API KEY. #### API KEY is disabled!

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ load_dotenv()
 # OPENAI_API_KEY = OPENAI_API_KEY
 ###################################################### GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 #client = genai.Client(api_key=GEMINI_API_KEY)
-genai.configure(api_key="AIzaSyBKtDEzVJgVXA4fc_LCzrcOYDxeQ8e43mg")
+genai.configure(api_key=GEMINI_API_KEY)
 
 # response = client.models.generate_content(
 #     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
